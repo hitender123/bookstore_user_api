c33bbe0789da0352c5e071c05fa7a4983fff187f
hitender123 <hitender.negi@gmail.com>
2026-05-30 23:12:35 +0530

    fix lint issue

--- utils/common/common.go
+++ utils/common/common.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"encoding/base64"
+	"fmt"
 	"image"
 	"io"
 	"log"
@@ -12,22 +13,28 @@ import (
 )
 
 func DecodeBase64(base64String string) ([]byte, error) {
-	return base64.StdEncoding.DecodeString(base64String)
+	data, err := base64.StdEncoding.DecodeString(base64String)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding base64 string: %w", err)
+	}
+	return data, nil
 }
 
 func SaveAsBMP(binaryData []byte, filename string) error {
 	file, err := os.Create(filename)
 	if err != nil {
-		return err
+		return fmt.Errorf("error creating file: %w", err)
 	}
 	defer SafeClose(file)
 
 	img, _, err := image.Decode(strings.NewReader(string(binaryData)))
 	if err != nil {
-		return err
+		return fmt.Errorf("error decode image: %w", err)
 	}
-
-	return bmp.Encode(file, img)
+	if err := bmp.Encode(file, img); err != nil {
+		return fmt.Errorf("encode image to bmp: %w", err)
+	}
+	return nil
 }
 
 func SafeClose(c io.Closer) {
